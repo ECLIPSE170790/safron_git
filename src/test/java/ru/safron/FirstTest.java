90a2b9edec4051a6034051248413ad5f906d80b2
ECLIPSE170790 <eclipse170790@mail.ru>
2021-10-24 16:50:20 +0600

    added first test

---
+++ src/test/java/ru/safron/FirstTest.java
@@ -0,0 +1,12 @@
+package ru.safron;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(true);
+    }
+}
